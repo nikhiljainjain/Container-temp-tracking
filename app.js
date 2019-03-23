14f791951e193b9aff30884e5a0865e3166da681
Nikhil Jain <nikhiljain.ashajain@gmail.com>
2019-03-23 16:57:32 +0000

    changes in app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var express = require("express");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var app = new express();
 var request = require("request");
 var sysX=true;
-app.set("view engine","ejs");
-app.use(express.static("public"));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/',function(req,res){
@@ -29,10 +40,26 @@ app.get('/',function(req,res){
             res.render("index", {data: {location: locationName, long: longitude, lat: latitude}});
     });
 });
+
 app.post("/rcv/data", function(req,res){
   console.log(req.body);
   res.send("ok");
 });
-app.listen(process.env.PORT,process.env.IP, function(){
-    console.log("Server started");
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
